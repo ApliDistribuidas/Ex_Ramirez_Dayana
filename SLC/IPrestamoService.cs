94bde329481045e9b09ac3f47fb923b5870d60d8
daramirez7 <93233944+ApliDistribuidas@users.noreply.github.com>
2024-12-13 19:02:22 -0500

    carpeta SLC

---
+++ SLC/IPrestamoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace SLC
+{
+    public interface IPrestamosService
+    {
+        PrestamosLibros CreateAsistentes(PrestamosLibros prestamoslibros);
+        bool Delete(int id);
+        List<PrestamosLibros> GetAll();
+        PrestamosLibros GetById(int id);
+        bool UpdateProduct(PrestamosLibros prestamoslibros);
+    }
+}
